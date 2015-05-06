a7f6aa52ee11b4aa9653f9e5cde5dd5536e661b5
Carolina <carolinapr86@gmail.com>
2015-05-06 20:19:04 +0000

    recipes almost ready!

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,5 @@ class Recipe < ActiveRecord::Base
     validates :chef_id, presence: true
     validates :name, presence: true, length: {minimum: 5, maximum: 100}
     validates :text, presence: true, length: {minimum: 10, maximum: 150}
-    validates :description, presence: true, length: {minimum: 20, maximum: 500}
+    validates :description, presence: true, length: {minimum: 20, maximum: 1500}
 end
