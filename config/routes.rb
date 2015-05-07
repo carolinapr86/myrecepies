7b023fcece34fecb8778aa26ab0034d21d79b820
Carolina <carolinapr86@gmail.com>
2015-05-07 22:39:10 +0000

    create chefs resources and add authentitacion, some security

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,13 @@ Rails.application.routes.draw do
   #get '/recipes/:id', to: 'recipes#show', as: 'recipe'
   #delete '/recipes/:id', to: 'recipes#destroy'
   
+  resources :chefs, except: [:new]
+  
+  get '/register/', to: 'chefs#new'
+  get '/login', to: 'logins#new'
+  post '/login', to: 'logins#create'
+  get '/logout/', to: 'logins#destroy'
+  
   resources :recipes do
     member do
       post 'like'
